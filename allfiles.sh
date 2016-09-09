30f2266dfe6f3c59f869597853806e443b01c5e9
rebecamoreno <rebecamorenoj@gmail.com>
2016-09-09 18:06:44 +0200

    Update allfiles.sh

--- allfiles.sh
+++ allfiles.sh
@@ -2,5 +2,5 @@
 while IFS='' read -r line || [[ -n "$line" ]]; do
     IFS=' ' read name url <<< "$line"
 	echo "Text read from file: $name $url"
-	wget --user='unhcr\morenoji' --password='J!menez4895&' $url -O $name
+	wget --user='unhcr\morenoji' --password='XXXXXXXX' $url -O $name
 done < "$1"
